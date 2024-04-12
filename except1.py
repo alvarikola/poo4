df337086f6b627b8b08b56bd8bae0d70f34adb99
alvarikola <alvaroe484@gmail.com>
2024-04-12 11:23:23 +0100

    Realizado todas las excepciones

---
+++ except1.py
@@ -0,0 +1,27 @@
+import random
+def intDiv(a:int, b:int)->int:
+    try:
+        return a // b
+    except TypeError:
+        print("TypeError Realizamos tareas de control de cierre")
+    except ZeroDivisionError:
+        print("ZeroDivisionError Realizamos tareas de control de cierre")
+    except Exception:
+        print("Exception Realizamos tareas de control de cierre")
+
+
+if __name__ == "__main__":
+    def main():
+        for i in range(30):
+            a = random.randint(0, 9)
+            b = random.randint(0, 9)
+            print("a: ", a, "b: ", b, "a // b", intDiv(a, b))
+    
+    intDiv(2, "3")
+
+    try:
+        intDiv()
+    except:
+        print("No lo consiguió")
+    
+    main()
