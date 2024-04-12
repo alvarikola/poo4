6322b54c2c44e3c7168fb78fc37110eb3e18eafd
alvarikola <alvaroe484@gmail.com>
2024-04-12 11:44:52 +0100

    Primera version de test_indexError.py

---
+++ test_indexError.py
@@ -0,0 +1,17 @@
+import pytest
+from indexError import lista
+
+
+def test_listaCorrecto():
+    assert lista(2) == "Pera"
+    assert lista(0) == "Manzana"
+
+
+
+def test_listaError():
+    assert lista(4) == IndexError
+
+
+
+
+
