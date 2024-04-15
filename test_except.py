2db7f68012a4a5399e9cff5abdb34299aff77548
alvarikola <alvaroe484@gmail.com>
2024-04-15 12:01:38 +0100

    avanzado en test_except.py

--- test_except.py
+++ test_except.py
@@ -1,7 +1,18 @@
 import pytest
 
+def divide(a, b):
+    if (b == 0):
+        raise ValueError("Cannot divide by zero")
+
+
 def test_exception():
     a = 0
     b = 10
     with pytest.raises(ZeroDivisionError):
         b / a
+
+
+def test_divide():
+    with pytest.raises(ValueError) as exception_info:
+        divide(10, 0)
+    assert str(exception_info.value).lower == "Cannot divide by zero".lower
