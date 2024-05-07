87edba12d03a979054338a7f42cea3c19039901e
alvarikola <alvaroe484@gmail.com>
2024-05-07 13:06:20 +0100

    primera version de anaPython.py

--- reg/anaPython.py
+++ reg/anaPython.py
@@ -1,4 +1,28 @@
+import re
+
 class AnaPython:
     @staticmethod
     def countDef(codigo:str) -> int:
-        pass
+        patron = "^def.+:$"
+        result = re.findall(patron, codigo) 
+        contador = 0
+        for i in range(len(result)):
+            contador += 1
+            print(i)
+        return contador
+    
+
+
+
+if __name__ == "__main__":
+    codigo = """
+def egsg():
+    pass
+    
+def aefga()
+    pass
+    
+efref:
+    pass
+"""
+    print(AnaPython.countDef(codigo))
