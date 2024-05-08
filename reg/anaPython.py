733f9a2d41290a36e2dcc82df02a4283392cea86
alvarikola <alvaroe484@gmail.com>
2024-05-08 13:14:15 +0100

    terminado encontrar variables

--- reg/anaPython.py
+++ reg/anaPython.py
@@ -6,16 +6,25 @@ class AnaPython:
         patron = "def\s+\w+\(\):"
         result = re.findall(patron, codigo)
         return f"Se han encontrado {len(result)} funciones"
+    
+
+    def encontrarVariable(codigo:str) ->list:
+        patron = "[a-zA-Z]+\s*=\s*\w+"
+        result = re.findall(patron, codigo)
+        return result
 
 if __name__ == "__main__":
     codigo = """
 def egsg():
-    pass
+    hola=1
     
 def aefga()
     pass
     
 efref:
     pass
+    
+adios = 1
 """
     print(AnaPython.countDef(codigo))
+    print(AnaPython.encontrarVariable(codigo))
